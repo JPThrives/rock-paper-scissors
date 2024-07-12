47b65e6e615de7f50e033d0fa647cee1f74a98c4
JPThrives <boruahjp1999@gmail.com>
2024-07-12 21:56:59 +0530

    created a function 'playround' to play one round of the game and declare the winner

--- script.js
+++ script.js
@@ -12,14 +12,74 @@ function getComputerChoice() {
 }
 
 function getHumanChoice() {
-    let HumanChoice = prompt("Enter your choice(rock/paper/scissor)");
-    if (HumanChoice === "rock" || HumanChoice === "paper" || HumanChoice === "scissor") {
-        return HumanChoice;
-    }
-    else {
-        return "Enter a valid choice";
+    while (true) {
+        let HumanChoice = prompt("Enter your choice(rock/paper/scissor)").toLowerCase();
+        if (HumanChoice === "rock" || HumanChoice === "paper" || HumanChoice === "scissors") {
+            return HumanChoice;
+        } 
+        else {
+            console.log("Enter a valid choice!");
+        }     
     }
 }
 
-console.log(getHumanChoice());
-console.log(getComputerChoice());
+
+let HumanScore = 0;
+let ComputerScore = 0;
+
+function playround(Humanselection,Computerselection) {
+
+    if (Humanselection == "rock") {
+        
+        switch (Computerselection) {
+            case "rock":
+                console.log("It's a tie!");
+                break;    
+            case "paper":
+                console.log("Computer wins!");
+                break;
+            case "scissors":
+                console.log("Congrats! You win!");
+                break;
+        }
+    }
+    else if (Humanselection == "paper") {
+        
+        switch (Computerselection) {
+            case "paper":
+                console.log("It's a tie!");
+                break;
+            case "scissors":
+                console.log("Computer wins!");
+                break;
+            case "rock":
+                console.log("Congrats! You win!");
+                break;        
+
+        }
+    }
+    else if (Humanselection == "scissors"){
+        switch (Computerselection) {
+            case "scissors":
+                console.log("It's a tie!");
+                break;
+            case "rock":
+                console.log("Computer wins!");
+                break;   
+            case "paper":
+                console.log("Congrats! You win!");
+                break;        
+
+        }
+    }
+    else {
+        return "Invalid";
+    }
+}
+
+let Humanselection = getHumanChoice();
+let Computerselection = getComputerChoice();
+
+console.log(`Player Choice: ${Humanselection}`);
+console.log(`Computer Choice: ${Computerselection}`);
+playround(Humanselection,Computerselection);
