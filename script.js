029220464a1f07de2abf0534a0eccf16ec33b089
JPThrives <boruahjp1999@gmail.com>
2024-07-12 20:20:14 +0530

    craeted a function in js named getComputerChoice to generate rock,paper and scissor randomly

---
+++ script.js
@@ -0,0 +1,14 @@
+function getComputerChoice() {
+    let ComputerChoice = Math.random();
+    if (ComputerChoice <= 0.33) {
+        return "rock";
+    }
+    else if (ComputerChoice > 0.33 && ComputerChoice <= 0.66) {
+        return "paper";
+    }
+    else {
+        return "scissors";
+    }
+}
+
+console.log(getComputerChoice());
